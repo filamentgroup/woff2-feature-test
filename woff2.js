837f67b6dffe737f252d94a7a9d3d5c8bd5d899c
Roel Nieskens <roel@pixelambacht.nl>
2016-08-20 10:46:01 +0200

    Fix WOFF2 font so it passes OTS in Firefox

--- woff2.js
+++ woff2.js
@@ -1,10 +1,10 @@
 var supportsWoff2 = (function( win ){
-	if( !( "FontFace" in win ) ) {
-		return false;
-	}
+  if( !( "FontFace" in win ) ) {
+    return false;
+  }
 
-	var f = new FontFace('t', 'url( "data:application/font-woff2;base64,d09GMgABAAAAAADcAAoAAAAAAggAAACWAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAABk4ALAoUNAE2AiQDCAsGAAQgBSAHIBtvAcieB3aD8wURQ+TZazbRE9HvF5vde4KCYGhiCgq/NKPF0i6UIsZynbP+Xi9Ng+XLbNlmNz/xIBBqq61FIQRJhC/+QA/08PJQJ3sK5TZFMlWzC/iK5GUN40psgqvxwBjBOg6JUSJ7ewyKE2AAaXZrfUB4v+hze37ugJ9d+DeYqiDwVgCawviwVFGnuttkLqIMGivmDg" ) format( "woff2" )', {});
-	f.load()['catch'](function() {});
+  var f = new FontFace('t', 'url( "data:application/font-woff2;base64,d09GMgABAAAAAADwAAoAAAAAAiQAAACoAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAABmAALAogOAE2AiQDBgsGAAQgBSAHIBuDAciO1EZ3I/mL5/+5/rfPnTt9/9Qa8H4cUUZxaRbh36LiKJoVh61XGzw6ufkpoeZBW4KphwFYIJGHB4LAY4hby++gW+6N1EN94I49v86yCpUdYgqeZrOWN34CMQg2tAmthdli0eePIwAKNIIRS4AGZFzdX9lbBUAQlm//f262/61o8PlYO/D1/X4FrWFFgdCQD9DpGJSxmFyjOAGUU4P0qigcNb82GAAA" ) format( "woff2" )', {});
+  f.load()['catch'](function() {});
 
-	return f.status == 'loading' || f.status == 'loaded';
+  return f.status == 'loading' || f.status == 'loaded';
 })( this );
